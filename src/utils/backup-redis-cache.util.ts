054d0459551d1b35fa276e350887955c13bb5ea3
Brian Patino <Patinobrian@gmail.com>
2023-09-05 16:50:12 -0400

    feat: create cache utils for processor downage

---
+++ src/utils/backup-redis-cache.util.ts
@@ -0,0 +1,110 @@
+import 'dotenv/config';
+import * as mongoose from 'mongoose';
+import redis from 'redis';
+import {
+  DiscordParticipant,
+  DiscordParticipantSchema,
+} from '../schemas/discord-participant.schema';
+import { CommunityEventSchema } from '../schemas/community-event.schema';
+
+/**
+ * Backup Redis Cache
+ * 1. Pull all active community events from MongoDB
+ * 2. Pull all participants keys using eventId
+ * 3. Pull all participants from Redis using keys
+ * 4. Backup participants to MongoDB
+ */
+const backupRedisCacheUtil = async () => {
+  console.log('attempting to backup redis cache');
+  const mongoConn = await mongoose.connect(process.env.MONGODB_URI);
+  const discordParticipantModel = mongoConn.model(
+    'discordParticipant',
+    DiscordParticipantSchema,
+  );
+  const communityEventModel = mongoConn.model(
+    'communityEvent',
+    CommunityEventSchema,
+  );
+  const communityEvents = await communityEventModel
+    .find({
+      isActive: true,
+    })
+    .exec();
+  console.log(`found ${communityEvents.length} active community events`);
+
+  if (communityEvents.length === 0) {
+    console.log('no active community events found');
+    process.exit(0);
+  }
+
+  const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT),
+  } as any);
+
+  redisClient.on('error', (err) => {
+    console.error('Redis error: ', err);
+  });
+
+  await redisClient.connect();
+  console.log('connected to redis');
+
+  const bulkWriteOps = [];
+  const endDate = new Date();
+
+  for (const event of communityEvents) {
+    const keys = await redisClient.keys(
+      `tracking:events:${event._id.toString()}:participants:*`,
+    );
+    console.log(
+      `found ${keys.length} participants for event ${event._id.toString()}`,
+    );
+    for (const redisKey of keys) {
+      const participant = JSON.parse(await redisClient.get(redisKey));
+      console.log(JSON.stringify(participant));
+
+      const discordParticipant = new DiscordParticipant();
+      discordParticipant.communityEvent = new mongoose.Types.ObjectId(
+        participant.eventId,
+      );
+      discordParticipant.userId = participant.userId;
+      discordParticipant.userTag = participant.userTag;
+      discordParticipant.startDate = new Date(participant.startDate);
+      discordParticipant.endDate = endDate;
+      discordParticipant.durationInMinutes =
+        (endDate.getTime() - discordParticipant.startDate.getTime()) /
+        1000 /
+        60;
+
+      bulkWriteOps.push({
+        updateOne: {
+          filter: {
+            communityEvent: discordParticipant.communityEvent,
+            userId: discordParticipant.userId,
+          },
+          update: discordParticipant,
+          upsert: true,
+        },
+      });
+    }
+  }
+
+  const result = await discordParticipantModel
+    .bulkWrite(bulkWriteOps, { ordered: true })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  console.log(result);
+
+  if (!result) {
+    throw new Error('failed to backup redis cache');
+  }
+
+  await mongoConn.disconnect();
+  await redisClient.disconnect();
+
+  console.log('backup of redis cache complete');
+};
+
+export { backupRedisCacheUtil };
---
+++ src/utils/load-redis-cache.util.ts
@@ -0,0 +1,88 @@
+import 'dotenv/config';
+import * as mongoose from 'mongoose';
+import redis from 'redis';
+import { DiscordParticipantSchema } from '../schemas/discord-participant.schema';
+import { CommunityEventSchema } from '../schemas/community-event.schema';
+import { DiscordParticipantDto } from '../dto/discord-participant.dto';
+
+/**
+ * Backup Redis Cache
+ * 1. Pull all active community events from MongoDB
+ * 2. Pull all participants keys using eventId
+ * 3. Pull all participants from Redis using keys
+ * 4. Backup participants to MongoDB
+ */
+const loadRedisCacheUtil = async () => {
+  console.log('attempting to load redis cache');
+  const mongoConn = await mongoose.connect(process.env.MONGODB_URI);
+  const discordParticipantModel = mongoConn.model(
+    'discordParticipant',
+    DiscordParticipantSchema,
+  );
+  const communityEventModel = mongoConn.model(
+    'communityEvent',
+    CommunityEventSchema,
+  );
+  const communityEvents = await communityEventModel
+    .find({
+      isActive: true,
+    })
+    .exec();
+  console.log(`found ${communityEvents.length} active community events`);
+
+  if (communityEvents.length === 0) {
+    console.log('no active community events found');
+    process.exit(0);
+  }
+
+  const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: parseInt(process.env.REDIS_PORT),
+  } as any);
+
+  redisClient.on('error', (err) => {
+    console.error('Redis error: ', err);
+  });
+
+  await redisClient.connect();
+  console.log('connected to redis');
+
+  for (const event of communityEvents) {
+    const participants = await discordParticipantModel
+      .find({
+        communityEvent: event._id,
+      })
+      .exec();
+
+    console.log(
+      `found ${
+        participants.length
+      } participants for event ${event._id.toString()}`,
+    );
+    for (const participant of participants) {
+      console.log(JSON.stringify(participant));
+
+      const cacheParticipant = new DiscordParticipantDto();
+      cacheParticipant.eventId = participant.communityEvent.toString();
+      cacheParticipant.userId = participant.userId;
+      cacheParticipant.userTag = participant.userTag;
+      cacheParticipant.startDate = participant.startDate;
+      cacheParticipant.endDate = participant.endDate;
+      cacheParticipant.durationInMinutes = participant.durationInMinutes;
+
+      await redisClient.set(
+        `tracking:events:${event._id.toString()}:participants:${
+          cacheParticipant.userId
+        }`,
+        JSON.stringify(cacheParticipant),
+      );
+    }
+  }
+
+  await mongoConn.disconnect();
+  await redisClient.disconnect();
+
+  console.log('load redis cache complete');
+};
+
+export { loadRedisCacheUtil };
