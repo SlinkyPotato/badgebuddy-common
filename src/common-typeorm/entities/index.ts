9e758402a038b38ee11ae4be22e49c3085be6de2
Brian Patino <Patinobrian@gmail.com>
2023-11-02 20:07:24 -0400

    refactor: create common config modules

---
+++ src/common-typeorm/entities/index.ts
@@ -0,0 +1,7 @@
+export { DiscordGuildEntity } from './discord-guild.entity';
+export { DiscordUserEntity } from './discord-user.entity';
+export { EventEntity } from './event.entity';
+export { EventDiscordDetailsEntity } from './event-discord-details.entity';
+export { EventDiscordParticipantEntity } from './event-discord-participant.entity';
+export { PoapLinkEntity } from './poap-link.entity';
+export { PoapLinkDiscordDetailsEntity } from './poap-link-discord-details.entity';
