bcf0c3af695bbe0b6d488f4183f17cd673cbd92a
SlinkyPotato <patinobrian@gmail.com>
2023-11-25 13:16:16 -0500

    docs: add mysql setup script

---
+++ src/common-typeorm/scripts/mysql.setup.sql
@@ -0,0 +1,46 @@
+CREATE DATABASE badge_buddy;
+DROP DATABASE badge_buddy;
+
+CREATE TABLE users
+(
+  id UUID PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(320) UNIQUE,
+  email_verified VARCHAR(28),
+  image TEXT
+);
+
+CREATE TABLE verification_tokens
+(
+  id UUID PRIMARY KEY,
+  identifier VARCHAR(320) NOT NULL,
+  expires VARCHAR(28) NOT NULL,
+  token VARCHAR(64) NOT NULL,
+  UNIQUE(identifier, token)
+);
+
+CREATE TABLE accounts
+(
+  id UUID PRIMARY KEY,
+  user_id UUID NOT NULL,
+  type VARCHAR(255) NOT NULL,
+  provider VARCHAR(255) NOT NULL,
+  provider_account_id VARCHAR(255) NOT NULL,
+  refresh_token TEXT,
+  access_token TEXT,
+  expires_at BIGINT UNSIGNED,
+  token_type VARCHAR(255),
+  scope TEXT,
+  id_token TEXT,
+  session_state TEXT,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE sessions
+(
+  id UUID PRIMARY KEY,
+  user_id UUID NOT NULL,
+  expires VARCHAR(28) NOT NULL,
+  session_token UUID NOT NULL UNIQUE,
+  FOREIGN KEY (user_id) REFERENCES users (id)
+);
