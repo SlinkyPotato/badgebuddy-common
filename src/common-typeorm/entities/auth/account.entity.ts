c9c6438f8e3ef9722c46564b42e086ad47a30a1b
SlinkyPotato <patinobrian@gmail.com>
2023-12-02 09:31:38 -0500

    fix

--- src/common-typeorm/entities/auth/account.entity.ts
+++ src/common-typeorm/entities/auth/account.entity.ts
@@ -31,7 +31,7 @@ export class AccountEntity {
   @ManyToOne(() => UserEntity, (user) => user.accounts, {
     createForeignKeyConstraints: true,
   })
-  user?: Relation<UserEntity>;
+  user: Relation<UserEntity>;
 
   @OneToMany(() => TokenEntity, (token) => token.accountId)
   tokens!: Relation<TokenEntity[]>;
