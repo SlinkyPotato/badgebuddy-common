97b40d3ab43334c4ae2818af53c331c67c1b6b42
SlinkyPotato <patinobrian@gmail.com>
2023-12-04 19:10:17 -0500

    fix: remove read only const property

--- src/common-typeorm/common-typeorm.providers.ts
+++ src/common-typeorm/common-typeorm.providers.ts
@@ -5,4 +5,4 @@ export const DiscordGuildBotSettingsRepositoryProvider = {
   provide: 'DISCORD_GUILD_BOT_SETTINGS_REPOSITORY',
   useFactory: (dataSource: DataSource) => dataSource.getRepository(DiscordGuildBotSettingsEntity),
   inject: ['DATA_SOURCE'],
-} as const;
+};
