8a6e83a8f68b775545a7129976330c3cac2df5dc
SlinkyPotato <patinobrian@gmail.com>
2023-12-10 17:11:57 -0500

    remove mongo

--- src/common-config/common-config.module.ts
+++ src/common-config/common-config.module.ts
@@ -15,7 +15,6 @@ export class CommonConfigModule {
             NODE_ENV: Joi.string().required(),
             REDIS_HOST: Joi.string().optional(),
             REDIS_PORT: Joi.number().optional(),
-            MONGODB_URI: Joi.string().optional(),
             LOGTAIL_TOKEN: Joi.string().optional(),
             DISCORD_BOT_TOKEN: Joi.string().required(),
             DISCORD_BOT_APPLICATION_ID: Joi.string().required(),
