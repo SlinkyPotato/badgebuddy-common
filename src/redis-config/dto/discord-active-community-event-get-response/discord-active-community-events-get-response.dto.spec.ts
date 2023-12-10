4393db877b215f463997913b8f9bf5cc469c73e6
SlinkyPotato <patinobrian@gmail.com>
2023-12-10 12:20:30 -0500

    feat: add DiscordActiveCommunityEvents response dto

---
+++ src/redis-config/dto/discord-active-community-event-get-response/discord-active-community-events-get-response.dto.spec.ts
@@ -0,0 +1,7 @@
+import { DiscordActiveCommunityEventsGetResponseDto } from './discord-active-community-events-get-response.dto';
+
+describe('DiscordActiveCommunityEventGetResponseDto', () => {
+  it('should be defined', () => {
+    expect(new DiscordActiveCommunityEventsGetResponseDto()).toBeDefined();
+  });
+});
