9e574d962f51fc8bd73fce03a7ec424cb769d8db
SlinkyPotato <patinobrian@gmail.com>
2023-12-11 09:18:56 -0500

    refactor: remove redis requirement from common

--- src/common-config/common-config.module.ts
+++ src/common-config/common-config.module.ts
@@ -13,8 +13,6 @@ export class CommonConfigModule {
           cache: true,
           validationSchema: Joi.object({
             NODE_ENV: Joi.string().required(),
-            REDIS_HOST: Joi.string().optional(),
-            REDIS_PORT: Joi.number().optional(),
             LOGTAIL_TOKEN: Joi.string().optional(),
             DISCORD_BOT_TOKEN: Joi.string().required(),
             DISCORD_BOT_APPLICATION_ID: Joi.string().required(),
@@ -23,7 +21,6 @@ export class CommonConfigModule {
             LOG_LEVEL: Joi.string()
               .required()
               .pattern(/^(fatal|error|warn|info|debug|trace)$/),
-            REDIS_CACHE_MIN: Joi.number().required(),
             ...options.validationSchema,
           }),
           validationOptions: {},
