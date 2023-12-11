3cb3c279c05c3d0d29e75a5937b5f5eee8c44599
SlinkyPotato <patinobrian@gmail.com>
2023-12-11 16:09:45 -0500

    refactor: remove common request object

--- src/badge-buddy-api-dto/community-events/discord-community-event-post-request/discord-community-event-post-request.dto.ts
+++ src/badge-buddy-api-dto/community-events/discord-community-event-post-request/discord-community-event-post-request.dto.ts
@@ -1,8 +1,21 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumberString, IsOptional, IsISO8601 } from 'class-validator';
-import { CommonManagementRequestDto } from '../common-management-request.dto';
 
-export class DiscordCommunityEventPostRequestDto extends CommonManagementRequestDto {
+export class DiscordCommunityEventPostRequestDto {
+  @ApiProperty({
+    required: true,
+    description: 'The snowflake ID of the guild',
+  })
+  @IsNumberString()
+  guildSId: string;
+
+  @ApiProperty({
+    required: true,
+    description: 'The snowflake ID of the organizer',
+  })
+  @IsNumberString()
+  organizerSId: string;
+
   @ApiProperty({
     required: true,
     description: 'The title of the event',
