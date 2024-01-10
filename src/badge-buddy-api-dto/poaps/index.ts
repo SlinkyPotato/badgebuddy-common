7fb7d6a264afb253a31f05987ded93703c103a0c
SlinkyPotato <patinobrian@gmail.com>
2024-01-10 13:57:21 -0500

    fix: add missing exports

--- src/badge-buddy-api-dto/poaps/index.ts
+++ src/badge-buddy-api-dto/poaps/index.ts
@@ -2,3 +2,5 @@ export { PoapsDistributeDiscordPostRequestDto } from './poaps-distribute-discord
 export { PoapsDistributeDiscordPostResponseDto } from './poaps-distribute-discord-post-response/poaps-distribute-discord-post-response.dto';
 export { PoapsStoreDiscordPostRequestDto } from './poaps-store-discord-post-request/poaps-store-discord-post-request.dto';
 export { PoapsStoreDiscordPostResponseDto } from './poaps-store-discord-post-response/poaps-store-discord-post-response.dto';
+export { PoapsClaimDiscordGetResponseDto } from './poaps-claim-discord-get-response/poaps-claim-discord-get-response.dto';
+export { PoapsClaimDiscordDto } from './poaps-claim-discord.dto';
