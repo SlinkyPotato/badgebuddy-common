1a84a9172ff253c0201ccd1cdd38adde2b0f8d07
SlinkyPotato <patinobrian@gmail.com>
2024-01-12 22:42:21 -0500

    refactor: remove discordSId from ProcessorToken

--- src/api-badgebuddy/auth/processor-token/processor-token-dto.ts
+++ src/api-badgebuddy/auth/processor-token/processor-token-dto.ts
@@ -1,7 +1,3 @@
 import { AccessTokenDto } from '../access-token/access-token.dto';
-import { IsNumberString } from 'class-validator';
 
-export class ProcessorTokenDto extends AccessTokenDto {
-  @IsNumberString()
-  discordUserSId: string;
-}
+export class ProcessorTokenDto extends AccessTokenDto {}
